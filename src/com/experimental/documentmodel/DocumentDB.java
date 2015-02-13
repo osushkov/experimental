c60234414037f79a5434d576326a7308e35685ed
Oleg Sushkov <osushkov@gmail.com>
2015-02-13 18:58:23 +1100

    blah

---
+++ src/com/experimental/documentmodel/DocumentDB.java
@@ -0,0 +1,55 @@
+package com.experimental.documentmodel;
+
+import com.google.common.base.Preconditions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by sushkov on 13/02/15.
+ */
+public class DocumentDB {
+
+  private Map<String, Document> documentMap = new HashMap<String, Document>();
+
+  public static DocumentDB instance = new DocumentDB();
+
+  private DocumentDB() {}
+
+  public WebsiteDocument createWebsiteDocument(String path) {
+    if (documentMap.containsKey(path)) {
+      Document doc = documentMap.get(path);
+      Preconditions.checkState(doc instanceof WebsiteDocument);
+      return (WebsiteDocument) doc;
+    } else {
+      WebsiteDocument doc = new WebsiteDocument(path);
+      documentMap.put(path, doc);
+      return doc;
+    }
+  }
+
+  public TopicalDocument createTopicalDocument(String path) {
+    if (documentMap.containsKey(path)) {
+      Document doc = documentMap.get(path);
+      Preconditions.checkState(doc instanceof TopicalDocument);
+      return (TopicalDocument) doc;
+    } else {
+      TopicalDocument doc = new TopicalDocument(path);
+      documentMap.put(path, doc);
+      return doc;
+    }
+  }
+
+  public TextCollectionDocument createTextCollectionDocument(String path) {
+    if (documentMap.containsKey(path)) {
+      Document doc = documentMap.get(path);
+      Preconditions.checkState(doc instanceof TextCollectionDocument);
+      return (TextCollectionDocument) doc;
+    } else {
+      TextCollectionDocument doc = new TextCollectionDocument(path);
+      documentMap.put(path, doc);
+      return doc;
+    }
+  }
+
+}
